d7afdf26b3787041e0b727bfe8bdf2d800a1208e
Mateusz Kulig <105542191+mateuszkulig@users.noreply.github.com>
2023-09-26 03:18:12 +0200

    testing cloudflare integration

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p> 
+          JAKUB KASPERSKI ZOSTAW MOJA STRONE W SPOKOJU
         </p>
         <a
           className="App-link"

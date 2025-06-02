999319519ec307b12592bfecc851788c9fbf95ba
Mateusz Kulig <mateusz.kulig02@gmail.com>
2025-06-02 15:01:27 +0200

    Remove texts

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> 
-          KOCHAM MOJA MALA NIUNIE
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Czy niunia uczyla sie juz dzisiaj Reacta???
-        </a>
       </header>
     </div>
   );
